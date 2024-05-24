a6e24c22dc89c3bfc57e6fe6db79a28d2664d782
ucaeon <yoocw0426@gmail.com>
2024-05-25 00:03:55 +0900

    ADD: section 6 코드 추가

---
+++ src/access/a/PublicClassInnerMain.java
@@ -0,0 +1,10 @@
+package access.a;
+
+public class PublicClassInnerMain {
+
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultClass1 defaultClass1 = new DefaultClass1();
+        DefaultClass2 defaultClass2 = new DefaultClass2();
+    }
+}
